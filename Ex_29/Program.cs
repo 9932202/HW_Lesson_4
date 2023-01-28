1ba063b8c74020806ebc125659a977a9e694cf58
Анастасия Казарина <ana-kazarina@MacBook-Pro-Anastasia.local>
2023-01-28 13:59:02 +0300

    попытка решить задачу 29

---
+++ Ex_29/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 29: Напишите программу, которая задаёт 
+массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] CreateArray(int numberOfArray){
+    int[] result = new int[numberOfArray];
+    for (int i = 0; i < numberOfArray; i++)
+    {
+        result[i] = new Random().Next(0, 100);
+    }
+    return result;
+}
+
+string GetStringArray(int[] array){
+    string result = "[";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result = result + $"{array[i]},";
+    }
+}
+
+int[] array = CreateArray(8);
+string stringArray = GetStringArray(array);
+Console.WriteLine(stringArray);
