20a76d633edad5b270d146f213a65ae9fddba8c7
Анастасия Казарина <ana-kazarina@MacBook-Pro-Anastasia.local>
2023-01-28 13:16:41 +0300

    решена задача 27

---
+++ Ex_25/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число 
+A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 
+*/
+
+int GetUserValue(string message){
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int GetExponentiationResult(int A, int B){
+    int res = A;
+    for (int i = 2; i <= B; i++){
+        res = res * A;
+    }
+    return res;
+}
+
+void ShowResult(string msg, int result){
+    Console.Write(msg);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(result);
+    Console.ResetColor();
+}
+
+int numberA = GetUserValue("Веедите число: ");
+int numberB = GetUserValue("Введите в какую степень возвести это число:  ");
+int result = GetExponentiationResult(numberA, numberB);
+ShowResult("Результат возведения в степень = ", result);
+
+//int result = GetExponentiationResult(2, 3);
+// Console.WriteLine(result);
---
+++ Ex_27/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 27: Напишите программу, 
+которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int GetUserValue(string message){
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+int GetMultipleResult(int number){
+int res = 0;
+while(number > 0){
+    res = res + number % 10;
+    number = number / 10;
+}
+return res;
+}
+
+void ShowResult(string msg, int result){
+    Console.Write(msg);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine(result);
+    Console.ResetColor();
+}
+
+int number = GetUserValue("Введите число");
+int result = GetMultipleResult(number);
+ShowResult($"Сумма цифр числа {number} равна : ", result);
+//Console.WriteLine(result);
